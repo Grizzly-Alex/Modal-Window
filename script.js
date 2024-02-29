a6bb8f0ecce2998f8a2c1c44a6b79c1d3fe14433
Alexander Medved <medved1991alexander@gmail.com>
2024-02-29 15:38:23 +0300

    Load base project

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnShowModalWindow = document.querySelectorAll('.show-modal-window');
+console.log(btnShowModalWindow);
+
