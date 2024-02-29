f1a855c0e1fe04914413989a69f058789a1e4fca
Alexander Medved <medved1991alexander@gmail.com>
2024-02-29 16:04:41 +0300

    Add ivent listener

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.close-modal-window');
 const btnShowModalWindow = document.querySelectorAll('.show-modal-window');
-console.log(btnShowModalWindow);
 
+const addHiddenClass = function (){
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const rmvHiddenClass = function (){
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+
+btnCloseModalWindow.addEventListener('click', addHiddenClass);
+
+overlay.addEventListener('click', addHiddenClass);
+
+for (let i=0; i < btnShowModalWindow.length; i++){
+    btnShowModalWindow[i].addEventListener('click', rmvHiddenClass);
+};
